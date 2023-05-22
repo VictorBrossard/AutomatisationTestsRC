78fd8709f0dd52dc5711a29475fb52021eec73dc
VictorBrossard <victor.brossard.pro@gmail.com>
2023-05-22 09:22:53 +0200

    Finalisation de l'ajout de l'utilisation de la commande -start #7

--- src/main/python/Interaction/Command.py
+++ src/main/python/Interaction/Command.py
@@ -15,6 +15,8 @@ from Interaction.Interaction import Interaction
 
 from Useful.UsefulFunction import get_program_list
 
+from FilesManagement.Files.ManageAnyFile import ManageAnyFile
+
 #-----------------------------------------------------------------------------------------------------
 
 class Command(object):
@@ -43,9 +45,11 @@ class Command(object):
             print("[ERREUR] La commande n'est pas bonne.")
             return
         
+        # help command
         if args[0] == "-help":
             self.__help_command()
 
+        # start command
         if args[0] == "-start":
             self.__start_command(args)
 
@@ -60,10 +64,12 @@ class Command(object):
 
         """
 
+        # verification of the number of arguments
         if len(args) > 2:
             print("[ERREUR] Il y a trop d'arguments.")
             return
         
+        # recovery of the lines of the file of the tests that we want to make
         try:
             fil = open(args[1], 'r')
             lines = fil.readlines()
@@ -71,17 +77,39 @@ class Command(object):
         except Exception as e:
             print("[ERREUR]", e)
             return
+        
+        # initialization of variables 
+        file_paths_list = []
+        new_file = ManageAnyFile()
 
-        for line in lines:
+        # decomposition of each line of the file
+        for i, line in enumerate(lines):
             decompose_line = line.split(";")
             is_correct = self.__check_informations_file(decompose_line)
 
+            # guard that prevents errors
             if not is_correct:
+                print(f"[ERREUR] Fichier mal rempli à la ligne {i}.")
                 return
-            
-            # à continuer
 
-        self.interaction.execute_test(self.database, [])
+            user_entry_list = [
+                decompose_line[2], # number of cards to produce
+                decompose_line[3], # number of cards made
+                decompose_line[4], # initialization of the machine
+                decompose_line[5]  # program to run
+            ]
+
+            # test folder path
+            test_file = open(f"{CONSTANT_TEST_AVAILABLE_FOLDER_PATH}\\{decompose_line[0]}.txt", 'r')
+            test_folder_path = test_file.readlines()[0].rstrip()
+            test_file.close()
+
+            # insertion of the test in the list according to the number put in the file
+            for j in range(0, int(decompose_line[1])):
+                file_paths_list.append(f"{CONSTANT_TEST_AVAILABLE_FOLDER_PATH}\\{decompose_line[0]}.txt")
+                new_file.create_file(test_folder_path, f"test_{j}.txt", user_entry_list)
+
+        self.interaction.execute_test(self.database, file_paths_list)
             
 
     def __check_informations_file(self, line: list[str]) -> bool:
@@ -93,12 +121,12 @@ class Command(object):
 
         try:
             return (
-                not os.path.exists(f"{CONSTANT_TEST_AVAILABLE_FOLDER_PATH}\\{line[0]}.txt") 
-                or int(line[1]) < 0 
-                or int(line[2]) < 0 
-                or int(line[3]) < 0
-                or line[4] == "Partielle"
-                or line[5] not in get_program_list()
+                os.path.exists(f"{CONSTANT_TEST_AVAILABLE_FOLDER_PATH}\\{line[0]}.txt") # name of the test file
+                and int(line[1]) > 0                                                    # number of test iterations
+                and int(line[2]) > 0                                                    # number of cards to produce
+                and int(line[3]) >= 0                                                   # number of cards made
+                and line[4] == "Partielle"                                              # initialization of the machine
+                and line[5] in get_program_list()                                       # program to run
             )
             
         except Exception as e:
